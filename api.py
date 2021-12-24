926f62c6162a230effc847ecad39a932cdb7a1b8
MEIakbar <94336036+MEIakbar@users.noreply.github.com>
2021-12-24 16:24:00 +0700

    Update api.py

--- api.py
+++ api.py
@@ -170,7 +170,7 @@ def get_url(url):
          'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'
      }
     r = requests.get(url, headers=headers)  # Using the custom headers we defined above
-    soup = BeautifulSoup(r.content, 'html5lib')
+    soup = BeautifulSoup(r.content, 'lxml')
     return soup
 
 
